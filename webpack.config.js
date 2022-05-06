4510df1cff63e37e5323938d733b281ff640bcf1
rodude123 <rodude123@gmail.com>
2022-05-06 10:13:54 +0100

    linked backend to the frontend using fetch method and use js to autoload the content to the page
    Signed-off-by: rodude123 <rodude123@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     watch: true,
-    entry: './lab_6/js/main.js',
+    entry: './lab_7/js/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'lab_4/js')
+        path: path.resolve(__dirname, '/srv/http/lab_7/js')
     },
 };
